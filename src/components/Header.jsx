f10c5d8618688545bbcc14240230a5cf3936323c
Kamal <kamaldeep734833@gmail.com>
2025-03-30 11:46:44 +0530

    add logout feature and username display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,53 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/Firebase";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const subscribe = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const handleSignOutClick = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div className="container flex justify-between items-center p-5">
+    <div className="container flex justify-between items-center">
       <div>
-        <NavLink to="/">
-          <img
-            className="h-20"
-            src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-            alt="Netflix Logo"
-          />
-        </NavLink>
+        <img
+          className="h-20"
+          src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="Netflix Logo"
+        />
       </div>
       <div className="flex text-white items-center ">
         <div className="p-2 font-bold">
-          <span>User Name</span>
+          <span>
+            {subscribe?.displayName ? subscribe.displayName : "Guest"}
+          </span>
         </div>
         <div className="flex justify-center items-center">
           <img
-            className="h-10 w-10 rounded-full object-cover"
-            src="../../Assets/Imeges/profilAvtar.jpg"
-            alt="Profile Avtar"
+            className="h-10 w-10 mr-2 ml-2 rounded-full object-cover"
+            src={subscribe?.photoURL ? subscribe.photoURL : "/ProfileAvtar.jpg"}
+            alt="Profile"
           />
         </div>
+        {subscribe?.uid && (
+          <div>
+            <button
+              onClick={handleSignOutClick}
+              className="bg-red-600 text-white font-bold py-2 px-4 rounded ml-2 m-2 cursor-pointer hover:scale-95"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
