1a97150a44d36cb62b4da05014951af57e006075
Kamal <kamaldeep734833@gmail.com>
2025-04-10 22:34:00 +0530

    update the constant component

--- src/utils/Constant.jsx
+++ src/utils/Constant.jsx
@@ -3,3 +3,12 @@ export const LOGO =
 export const BACKGROUND_IMAGE =
   "url('https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_medium.jpg')";
 export const PROFILE_AVATAR = "../public/ProfileAvtar.jpg";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzI1NzE0MDc0MGU0YTgyNGI1ZWQ1YzY3NjE4Nzk1MiIsIm5iZiI6MTcwODQ4NTkwMC41NDU5OTk4LCJzdWIiOiI2NWQ1NmQwY2ZmZDQ0ZDAxNDkyYWI0MmYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.pZq6dWf03PIAg2KR1HHEGLlBh_xQdPaX7zAPnKcW48g",
+  },
+};
