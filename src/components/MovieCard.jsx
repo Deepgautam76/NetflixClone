da071234c9ea52f0412f1c21dd3ea33b354fdbd1
Kamal <kamaldeep734833@gmail.com>
2025-04-13 11:36:53 +0530

    build the card and list looking good add some css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { IMAGE_CDN_URL } from "../utils/Constant";
 
-const MovieCard = ({ poster }) => {
+//Here is the only build the moviecard take the poster path and return the image
+const MovieCard = ({ posterPath }) => {
   return (
-    <div className="mx-2">
-      <img className="w-48" src={IMAGE_CDN_URL + poster} alt="Movie poster" />
+    <div className="w-48 mr-4 ">
+      <img src={IMAGE_CDN_URL + posterPath} alt="Movie poster" />
     </div>
   );
 };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,15 +3,15 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   return (
-    <div>
-      <div>
-        <h1 className="text-2xl font-bold mx-4 p-2">{title}</h1>
+    <div className="px-4">
+      <h1 className="text-2xl font-bold py-1">{title}</h1>
+      <div className="flex overflow-x-scroll scroll-smooth whitespace-nowrap">
         <div className="flex">
-          {movies !== null
-            ? movies.map((movie) => (
-                <MovieCard key={movie?.id} poster={movie?.poster_path} />
-              ))
-            : "Loading..."}
+          {movies === null
+            ? "Loading..."
+            : movies?.map((movie) => (
+                <MovieCard key={movie?.id} posterPath={movie?.poster_path} />
+              ))}
         </div>
       </div>
     </div>
