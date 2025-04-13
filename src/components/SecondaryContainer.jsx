7131386a49110d6dc77846a860be54ad51bc43cc
Kamal <kamaldeep734833@gmail.com>
2025-04-13 11:54:59 +0530

    Build the more list of movies

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -4,9 +4,13 @@ import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
+  // Here make the movieList more dynamic
   return (
-    <div>
+    <div className="-mt-50 relative z-50">
       <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"Trending Movies"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"Horror Movies"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"UpComing Movies"} movies={movies?.nowPlayingMovies} />
     </div>
   );
 };
