8a2b970cad2ca7f5d8d10c6c7ad612aa146bef8d
Kamal <kamaldeep734833@gmail.com>
2025-04-17 23:21:51 +0530

    add code for calling new custom hooks

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -3,10 +3,12 @@ import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import useTrendingMovies from "../hooks/useTrendingMovies";
 
 const Browse = () => {
   // Called custom hooks
   useNowPlayingMovies();
+  useTrendingMovies();
 
   return (
     <div className="container">
