ff4ef0721c0e33e339fafc47b7d596b15b891477
Kamal <kamaldeep734833@gmail.com>
2025-04-18 23:07:43 +0530

    build the custom hooks for popular movies

---
+++ src/hooks/usePopularMovies.jsx
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/Constant";
+import {addPopularMovies } from "../utils/movieSlice";
+import { useEffect, useState } from "react";
+
+const usePopularMovies= () => {
+  const [moviesData, setMoviesData] = useState([]);
+  const dispatch = useDispatch();
+  const YOUR_API_KEY = import.meta.env.VITE_YOUR_API_KEY;
+  const movieData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${YOUR_API_KEY}&language=en-US`,
+        API_OPTIONS
+      );
+      const data = await response?.json();
+      //Set movies into moviesSlice
+      dispatch(addPopularMovies(data?.results));
+      setMoviesData(data?.results);
+    } catch (error) {
+      console.error("Error fetching movie data of trending movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    const clenup = movieData();
+    return () => {
+      clenup;
+    };
+  }, []);
+  return moviesData;
+};
+
+export default usePopularMovies;
